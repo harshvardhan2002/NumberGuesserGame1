7f5a570ace19fe13e0dabfa9cf9592d92ae9fb43
Harshvardhan Gupta <76556011+harshvardhan2002@users.noreply.github.com>
2024-09-30 19:30:45 +0530

    Add files via upload

---
+++ NumberGuesserGame1/NumberGuesser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuesserGame1
+{
+    class NumberGuesser
+    {
+        private int minRange;
+        private int maxRange;
+        private int secretNumber;
+
+        public NumberGuesser(int minRange, int maxRange, int secretNumber)
+        {
+            this.minRange = minRange;
+            this.maxRange = maxRange;
+            this.secretNumber = secretNumber;
+        }
+        public void StartGuessing()
+        {
+            Console.Clear();
+            Console.WriteLine("Range set by game master is " + minRange + "-" + maxRange);
+            Console.WriteLine("Guess the number: ");
+
+            int guessingNumber = Convert.ToInt32(Console.ReadLine());
+
+            while (guessingNumber != secretNumber)
+            {
+                if (guessingNumber < secretNumber)
+                {
+                    Console.WriteLine("The number is too low. Try again!");
+                }
+                else
+                {
+                    Console.WriteLine("The number is too high. Try again!");
+                }
+
+                guessingNumber = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Congrats! The number is right!");
+        }
+
+    }
+}
---
+++ NumberGuesserGame1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace NumberGuesserGame1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber;
+
+            int minRange, maxRange;
+
+            Console.WriteLine("Enter the minimum range: ");
+            minRange = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the maximum range: ");
+            maxRange = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the secret number: (within the range)");
+            secretNumber = Convert.ToInt32(Console.ReadLine());
+
+            NumberGuesser numberGuesser = new NumberGuesser(minRange, maxRange, secretNumber);
+
+            numberGuesser.StartGuessing();
+        }
+    }
+}
